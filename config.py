8d0f193f7a8a993a8023c99e6191d7ce1b4a9668
dwihandi <nerradfour@gmail.com>
2020-12-24 05:37:14 +0700

    heroku deployment changes

--- config.py
+++ config.py
@@ -4,4 +4,8 @@ class Config:
     SECRET_KEY = os.urandom(64)
     SESSION_TYPE = 'filesystem'
     SESSION_FILE_DIR = './.flask_session/'
+
+    # for heroku logs
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
     
