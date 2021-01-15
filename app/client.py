0f98378c9b4bf71b165acda7ed634bb9ec38596d
dwihandi <nerradfour@gmail.com>
2021-01-15 19:09:54 +0700

    added gitignore

---
+++ app/client.py
@@ -0,0 +1,4 @@
+client_id =  'your spotify developer client id'
+client_secret = 'your spotify developer client secret'
+redirect_uri = 'your host uri'
+scope = 'user-read-currently-playing playlist-modify-private user-top-read user-library-read user-read-recently-played playlist-modify-public'
